6b8e51cd3068bd702d91bbd3f8c938c00c67d5a4
n8ss <92551221+n8ss@users.noreply.github.com>
2022-03-07 14:30:08 -0500

    yeet

--- PC_GUI/hangman_gui/main.py
+++ PC_GUI/hangman_gui/main.py
@@ -1,8 +1,27 @@
 from tkinter import *
+from tkinter import messagebox
 
 # window things
 window = Tk()
 window.title('hangman')
 window.configure(width=1300, height=800)
 window.configure(bg='lightgray')
+
+
+
+btn1 = Button(window, text="A",bg="grey", fg="white",width=3,height=1,font=('Helvetica','20'),command=lambda: clicked("A"))
+btn1.grid(column=1, row=1)
+
+
+messagebox.showinfo("Congratulations!!!!", "Your answer is correct")
+
+
+
+
+
+
+
 window.mainloop()
+
+
+
