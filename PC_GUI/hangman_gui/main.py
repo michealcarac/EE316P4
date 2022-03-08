2a2eb5be2aad4b8cc77c3f13a97045c61c83577d
n8ss <92551221+n8ss@users.noreply.github.com>
2022-03-07 19:04:54 -0500

    ffafda

--- PC_GUI/hangman_gui/main.py
+++ PC_GUI/hangman_gui/main.py
@@ -1,27 +1,77 @@
+import string
+import tkinter
 from tkinter import *
-from tkinter import messagebox
+from tkinter import messagebox, Tk, Entry
+import pygame
+import random
 
-# window things
+
+def wordSelect():
+    # word selection out of .txt file
+    index = random.randint(0, 7776)
+    file = open('wordlist.txt', 'r')
+    lines = file.readlines()
+    selected_word = lines[index].strip('\n')
+    print(selected_word)
+
+    # creating blanks for word
+    x = 300
+    for i in range(0, len(selected_word)):
+        x += 60
+        exec('d{}=Label(window,text="_",bg="#E7FFFF",font=("arial",50))'.format(i))
+        exec('d{}.place(x={},y={})'.format(i, x, 700))
+
+
+
+def keyCheck(event):
+    if event.keysym == "Y":
+        print("it was Y")
+        checkRuns = True
+        newGame.destroy()
+        wordSelect()
+
+
+
+# def endCheck(event):
+#     if event.keysym == "Y":
+#         print("it was Y")
+#         play = True
+#         window.destroy()
+#     elif event.keysym == "y":
+#         print("it was Y")
+#         play = True
+#         window.destroy()
+#     elif event.keysym == "N":
+#         print("it was N")
+#         play = False
+#         exit()
+#     elif event.keysym == "n":
+#         print("it was n")
+#         play = False
+#         exit()
+
+
+# window setup
 window = Tk()
 window.title('hangman')
-window.configure(width=1300, height=800)
-window.configure(bg='lightgray')
+window.configure(width=1200, height=800)
+window.resizable(False, False)
+window.configure(bg='gray')
+
+# music for no reason
+pygame.mixer.init()
+pygame.mixer.music.load("wii_music.mp3")
+pygame.mixer.music.play(-1)
+
+# asking to start game
+newGame = Label(window, text="New Game? Enter Y to play", font=("arial", 80))
+newGame.place(x=100, y=300)
+
+# read key pressed for startup
+
+window.bind("<Key>", keyCheck)
 
 
-
-btn1 = Button(window, text="A",bg="grey", fg="white",width=3,height=1,font=('Helvetica','20'),command=lambda: clicked("A"))
-btn1.grid(column=1, row=1)
-
-
-messagebox.showinfo("Congratulations!!!!", "Your answer is correct")
-
-
-
-
-
-
+guessCount = 0
 
 window.mainloop()
-
-
-
